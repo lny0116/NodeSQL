b38da6c094c69e02199fbf3111e5806196bc46c8
lena <lny970116@gmail.com>
2021-04-06 19:21:14 +0900

    6

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ var app = http.createServer(function(request,response){
           db.query(`SELECT * FROM topic`, function (error, topics) {
               if (error){
                   throw error;
-              }
+              } //문제가 있다면 행위를 던져버려라(중단) 없다면 response 함
               db.query(`SELECT * FROM topic WHERE id = ?`, [queryData.id], function (error2, topic) { //queryData.id의 값이 ?를 통해 치환되어 자동으로 들어감. 이는 공격 의도가 있는 코드를 세탁해주는 일을 해준다. [이렇게 사용하기!]
                   if (error2){
                       throw error2;
@@ -51,11 +51,11 @@ var app = http.createServer(function(request,response){
           });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
-          <form action="/create_process" method="post">
+        db.query(`SELECT * FROM topic`, function (error, topics) {
+            var title = 'Create';
+            var list = template.list(topics); // lib/template.js의 list
+            var html = template.HTML(title, list, // lib/template.js의 html
+                `<form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -63,11 +63,12 @@ var app = http.createServer(function(request,response){
             <p>
               <input type="submit">
             </p>
-          </form>
-        `, '');
-        response.writeHead(200);
-        response.end(html);
-      });
+          </form>`,
+                `<a href="/create">create</a>`
+            );
+            response.writeHead(200);
+            response.end(html);
+        });
     } else if(pathname === '/create_process'){
       var body = '';
       request.on('data', function(data){
@@ -75,11 +76,12 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var title = post.title;
-          var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
+          db.query(`INSERT INTO topic (title, description, created, author_id) VALUES (?, ?, NOW(), ?)`, [post.title, post.description, 1], function (error, result){
+              if (error){
+                  throw (error);
+              }
+              response.writeHead(302, {Location: `/?id=${result.insertId}`}); //삽입된 행의 id값을 가져오기
+              response.end();
           })
       });
     } else if(pathname === '/update'){
